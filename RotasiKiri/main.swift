5567ec39adc2c386f45a96dca04b8d6b711432e1
Habib Akbar <habibakbar360@gmail.com>
2021-06-03 19:21:24 +0700

    add helper comments

--- RotasiKiri/main.swift
+++ RotasiKiri/main.swift
@@ -7,5 +7,61 @@
 
 import Foundation
 
-print("Hello, World!")
+func getInputArray(message: String) -> [Int] {
+    
+    print(message, terminator: ": ")
+    let line = readLine() ?? ""
+    
+    return line.split(separator: " ").map{ Int($0) ?? 0}
+    
+}
+
+func getInputNumber(message: String) -> Int {
+    
+    print(message, terminator: ": ")
+    
+    return Int(readLine() ?? "") ?? 0
+}
+
+//MARK: - Main Logic
+
+func rotateLeft(arrayInt: [Int], numOfRotation: Int) {
+    
+    var array = arrayInt
+    
+    // to avoid unnecessary rotation, rotation value is ecual number of rotation modulo array size
+    let rotation = numOfRotation % arrayInt.count
+    
+    // if rotation is zero, its rotated array will look like the original array
+    if rotation == 0 {
+        print("Rotated array \(array)")
+        return
+    }
+    
+    // rotate array base on rotation value
+    for _ in 0..<rotation {
+        let first = array.removeFirst()
+        array.append(first)
+    }
+    
+    print("Rotated array \(array)")
+}
+
+var shuldRepeat = 1
+
+repeat {
+    
+    let array = getInputArray(message: "Masukkan angka array (angka dipisah dengan sepasi)")
+    let rotation = getInputNumber(message: "Massukkan angka rotation")
+
+    print("Original array: \(array)")
+    print("Number of rotation: \(rotation)")
+
+    rotateLeft(arrayInt: array, numOfRotation: rotation)
+    
+    shuldRepeat = getInputNumber(message: "Apakah anda ingin mencoba lagi? (masukkan angka 1 untuk coba lagi)") != 1 ? 0 : 1
+    
+} while shuldRepeat != 0
+
+
 
